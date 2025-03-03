907b7c4791e5e469a4d37063ac303845984919d4
carloshermoso64 <carloshermoso29@hotmail.com>
2025-03-03 08:36:55 +0000

    Modified success page

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,8 +1,9 @@
 export default function PaymentSuccess({
-    searchParams: { amount },
+    searchParams,
   }: {
     searchParams: { amount: string };
   }) {
+    const { amount } = searchParams;
     return (
       <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
         <div className="mb-10">
