419679fe1241cbd943d4bf9016e57b3135a0f646
carloshermoso64 <carloshermoso29@hotmail.com>
2025-03-04 08:53:39 +0000

    Modified success page await searchparams

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,26 +1,19 @@
-import { notFound } from "next/navigation";
-
 export default async function PaymentSuccess({
-  searchParams,
-}: {
-  searchParams: Awaited<Promise<Record<string, string | string[] | undefined>>>;
-}) {
-  if (!searchParams || typeof searchParams !== "object") {
-    return notFound();
-  }
-
-  const amount = typeof searchParams.amount === "string" ? searchParams.amount : "0.00"; // Ensure amount is a string
-
-  return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
-      <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-        <h2 className="text-2xl">You successfully sent</h2>
-
-        <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
-          €{amount}
+    searchParams,
+  }: {
+    searchParams: Promise<{ amount: string }>;
+  }) {
+    const { amount } = await searchParams;
+    return (
+      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+        <div className="mb-10">
+          <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
+          <h2 className="text-2xl">You successfully sent</h2>
+  
+          <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
+            ${amount}
+          </div>
         </div>
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  }
